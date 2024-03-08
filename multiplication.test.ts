09459e78a06ad79a171eb1bc889e8aee502309bb
loody7 <ajhappy12@gmail.com>
2024-03-08 17:21:25 +0900

    remove side-effect in times method

--- multiplication.test.ts
+++ multiplication.test.ts
@@ -1,9 +1,11 @@
 import { Dollar } from "./dollar";
 
 describe("달러 배수 테스트", () => {
-    it("5달러에 2를 곱하면 10달러가 된다", () => {
+    it("", () => {
         const five = new Dollar(5);
-        five.times(2);
-        expect(five.amount).toBe(10);
+        let product = five.times(2);
+        expect(product.amount).toBe(10);
+        product = five.times(3);
+        expect(product.amount).toBe(15);
     })
 })
